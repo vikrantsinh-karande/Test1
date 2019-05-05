4717c7fc33b5bb240d513ea27060a74bb61c7757
vikrantsinh-karande <viksskarande@gmail.com>
2019-05-04 21:47:55 -0500

    This is my first commit

---
+++ src/main/java/HelloVikss/HelloVikss/App.java
@@ -0,0 +1,14 @@
+package HelloVikss.HelloVikss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("I have complete faith in you Vikss.\nJust have fun !");
+		
+	}
+}
